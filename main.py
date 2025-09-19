56f3edc7c86cc57fc8c7fa316f05f29b5e754af6
Saketh <saketh.medicherla@gmail.com>
2025-09-19 15:19:12 -0400

    Added Player class

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_KINDS, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS, ASTEROID_SPAWN_RATE
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_KINDS, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS, ASTEROID_SPAWN_RATE, PLAYER_RADIUS
+from player import Player
 import pygame
 
 def main():
     pygame.init()
+    dt = 0
+    clk = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clk.tick(60) / 1000
 
 
 if __name__ == "__main__":
