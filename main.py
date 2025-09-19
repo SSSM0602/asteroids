f9d021acebc7a97c03dd9efc7aaf7b55cab61fb2
Saketh <saketh.medicherla@gmail.com>
2025-09-18 22:00:57 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_KINDS, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS, ASTEROID_SPAWN_RATE
+import pygame
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
